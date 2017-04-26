b23e81cf7e144da3ad31467f376d8ba394926fae
Cheng Jiang <cheng.jiang@sap.com>
2017-04-26 11:57:15 +0800

    Add project files.

---
+++ Configuration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Newtonsoft.Json;
+
+namespace SqlBulkCopyRunner.Model
+{
+    [Serializable]
+    public class Configuration
+    {
+        public List<string> tables;
+        // @"Server=localhost;Database=WIP;Trusted_Connection=true;User ID=WIP;Password=<password>"
+        public string sourceConnectionString;
+        public string targetConnectionString;
+
+        public int batchSize;
+        public int notifyAfter;
+
+        private static string CONFIG_FILE = "config.json";
+
+        public static Configuration Load()
+        {
+            try
+            {
+                string configContent = File.ReadAllText(CONFIG_FILE);
+                Configuration config = JsonConvert.DeserializeObject<Configuration>(configContent);
+                return config;
+            }
+            catch (Exception)
+            {
+                Console.Error.WriteLine("Exception thrown when reading configuration file.");
+                return new Configuration {
+                    batchSize = -1  // invalid value which is <= 0
+                };
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+using SqlBulkCopyRunner.Model;
+
+namespace SqlBulkCopyRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Configuration config = Configuration.Load();
+
+            if (config.batchSize <= 0) {
+                Console.Error.WriteLine("Incorrect configuration file content. Please check and adjust it.");
+                return;
+            }
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            foreach (string table in config.tables) {
+                Console.Out.WriteLine("Now copying table: {0}", table);
+                bulkCopy(config.sourceConnectionString, config.targetConnectionString, table, config.batchSize, config.notifyAfter);
+                //System.Threading.Thread.Sleep(2000);
+                Console.Out.WriteLine("");
+            }
+
+            stopwatch.Stop();
+            long elapsedTime = stopwatch.ElapsedMilliseconds;
+            Console.Out.WriteLine("Execution completed in {0} seconds.", elapsedTime / 1000);
+        }
+
+        private static void bulkCopy(string sourceConnectionString, string targetConnectionString, string table, int batchSize, int notifyAfter)
+        {
+            // get the source data
+            using (SqlConnection sourceConnection =
+                    new SqlConnection(sourceConnectionString)) {
+                SqlCommand sql = new SqlCommand("SELECT * FROM " + table, sourceConnection);
+                sourceConnection.Open();
+                SqlDataReader reader = sql.ExecuteReader();
+
+                // open the destination data
+                using (SqlConnection destinationConnection =
+                            new SqlConnection(targetConnectionString)) {
+                    // open the connection
+                    destinationConnection.Open();
+
+                    using (SqlBulkCopy bulkCopy =
+                    new SqlBulkCopy(destinationConnection.ConnectionString)) {
+                        bulkCopy.BatchSize = batchSize;
+                        bulkCopy.NotifyAfter = notifyAfter;
+                        bulkCopy.SqlRowsCopied +=
+                            new SqlRowsCopiedEventHandler(bulkCopy_SqlRowsCopied);
+                        bulkCopy.DestinationTableName = table;
+                        bulkCopy.WriteToServer(reader);
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+        private static void bulkCopy_SqlRowsCopied(object sender, SqlRowsCopiedEventArgs e)
+        {
+            Console.Write("=");
+        }
+
+    }
+}
